018d9cf6b45cc4ac638532e1bac8b04fdbfd22b3
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-06-25 15:20:45 +0800

    June 25 2024 3:21pm

---
+++ vite-project/src/components/mainRowOne/aboutChildContent/CorporateProfileOverview.jsx
@@ -0,0 +1,13 @@
+const CorporateProfileOverview = () => {
+  return (
+    <>
+      <div>CorporateProfileOverview</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe maiores
+        eligendi dolorem aut alias ipsa.
+      </p>
+    </>
+  );
+};
+
+export default CorporateProfileOverview;
