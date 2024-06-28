3fe738130ec98620f38268d30969f88f2f0e3b60
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-06-28 10:42:59 +0800

    June 28 2024 10:43am

--- vite-project/src/components/NavbarTwo.jsx
+++ vite-project/src/components/NavbarTwo.jsx
@@ -1,6 +1,6 @@
 import "../components/mainRowTwo/NavbarTwoStyle.css";
 import BDOlogo1 from "../components/images/bdoLogo.svg";
-import SearchIcon from "../components/images/searchicon.svg";
+import SearchIcon from "../components/images/search-icon.png";
 import ApplyIcon from "../components/images/apply-icon.png";
 import loginIcon from "../components/images/loginicon.png";
 import { Link } from "react-router-dom";
@@ -14,34 +14,117 @@ const NavbarTwo = () => {
         </div>
 
         {/* {---------------personal---------------} */}
-        <div>
-          <h4>
-            <Link>Personal</Link>
-          </h4>
+        <div className="personal">
+          <p>
+            <Link
+              style={{
+                textDecoration: "none",
+                fontSize: "18px",
+                color: "white",
+                fontFamily: "Nunito",
+                position: "relative",
+                top: "8px",
+              }}
+            >
+              Personal
+            </Link>
+          </p>
         </div>
         {/* {---------------Business---------------} */}
-        <div>
+        <div className="business">
           <h4>
-            <Link style={{ color: "orange" }}>Business</Link>
+            <Link
+              style={{
+                color: "#FDB913",
+                textDecoration: "none",
+                fontSize: "18px",
+                fontFamily: "Nunito",
+                fontWeight: "700",
+              }}
+            >
+              Business
+            </Link>
           </h4>
         </div>
-        <div className="search" style={{ display: "flex", height: "50px" }}>
-          <img src={SearchIcon} alt="searchIcon" />
-          <h5>
-            <Link>Search</Link>
-          </h5>
-        </div>
-        <div className="apply" style={{ display: "flex", height: "50px" }}>
-          <img src={ApplyIcon} alt="applyIcon" />
-          <h5>
-            <Link>Apply</Link>
-          </h5>
-        </div>
-        <div style={{ display: "flex", height: "50px" }}>
-          <img src={loginIcon} alt="loginIcon" />
-          <h5>
-            <Link>Login</Link>
-          </h5>
+
+        {/* {-------------------Search****Apply****Login Area----------------} */}
+        <div className="searchApplyLogin">
+          <div className="search" style={{ display: "flex" }}>
+            <img
+              src={SearchIcon}
+              width={33}
+              height={33}
+              alt="searchIcon"
+              style={{ position: "relative", top: "4px" }}
+            />
+            <p>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  fontSize: "18px",
+                  fontWeight: "400",
+                  fontFamily: "Nunito",
+                  paddingLeft: "20px",
+                  position: "relative",
+                  top: "7px",
+                }}
+              >
+                Search
+              </Link>
+            </p>
+          </div>
+          <div className="apply" style={{ display: "flex" }}>
+            <img
+              src={ApplyIcon}
+              width={33}
+              height={33}
+              alt="applyIcon"
+              style={{ position: "relative", top: "4px" }}
+            />
+            <p>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  fontSize: "18px",
+                  fontWeight: "400",
+                  fontFamily: "Nunito",
+                  paddingLeft: "20px",
+                  position: "relative",
+                  top: "7px",
+                }}
+              >
+                Apply
+              </Link>
+            </p>
+          </div>
+          <div className="logIn" style={{ display: "flex" }}>
+            <img
+              src={loginIcon}
+              width={33}
+              height={33}
+              alt="loginIcon"
+              style={{ position: "relative", top: "4px" }}
+            />
+            <p>
+              <Link
+                style={{
+                  textDecoration: "none",
+                  color: "white",
+                  fontSize: "18px",
+                  fontWeight: "400",
+                  fontFamily: "Nunito",
+                  paddingLeft: "20px",
+                  position: "relative",
+                  top: "7px",
+                }}
+              >
+                Login
+              </Link>
+            </p>
+            <i className="fa-solid fa-caret-down one"></i>
+          </div>
         </div>
       </div>
     </>
