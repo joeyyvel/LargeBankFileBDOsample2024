e2efc885dec753ef736eb7b4b0c1f36ccb7ecaa3
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-07-05 14:18:03 +0800

    July 5 2024 2:18pm

--- vite-project/src/components/businessRow/BusinessPage.jsx
+++ vite-project/src/components/businessRow/BusinessPage.jsx
@@ -1,11 +1,27 @@
 import welcomeToBDOBusiness from "../images/welcomeToBDOBusiness.png";
 import NavbarOne from "../NavbarOne";
 import NavbarTwo from "../NavbarTwo";
+import { BusinessMenuItem } from "./BusinessMenuItem";
+import "../businessRow/BusinessMainPageStyle.css";
+import { Link } from "react-router-dom";
 const BusinessPage = () => {
   return (
     <>
       <NavbarOne />
       <NavbarTwo />
+      <div className="busMenu">
+        <ul className="businessMenu">
+          {BusinessMenuItem.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link className={item.cName} to={item.url}>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <img src={welcomeToBDOBusiness} alt="" />
     </>
   );
--- vite-project/src/routes/PersonalMainPage.jsx
+++ vite-project/src/routes/PersonalMainPage.jsx
@@ -1,13 +1,33 @@
 import personalPic from "../components/images/welcomeToBDOpersonal.png";
 import NavbarOne from "../components/NavbarOne";
 import NavbarTwo from "../components/NavbarTwo";
+import { PersonalMenuItem } from "../components/personalRow/PersonalMenuItem";
+import { Link } from "react-router-dom";
+import "../components/personalRow/PersonalMainPageStyle.css";
 
 const PersonalMainPage = () => {
   return (
     <>
       <NavbarOne />
       <NavbarTwo />
-      <div>PersonalMainPage</div>
+
+      <div className="perMenu">
+        <ul className="personalMenu">
+          {PersonalMenuItem.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link
+                  className={item.cName}
+                  to={item.url}
+                  style={{ textDecoration: "none" }}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <div>
         <img src={personalPic} alt="" />
       </div>
